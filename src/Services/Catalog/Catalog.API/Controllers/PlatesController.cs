6ec2edcaecafdcdd1c6afc44ad255182fad2df50
Daniel Tregoiing <daniel.tregoiing@sgsco.com>
2024-06-09 19:14:53 +0100

    Added Search functionality for letters and numbers Added supporting tests

--- src/Services/Catalog/Catalog.API/Controllers/PlatesController.cs
+++ src/Services/Catalog/Catalog.API/Controllers/PlatesController.cs
@@ -16,11 +16,12 @@ public class PlatesController : Controller
 	}
 
 	[HttpGet("GetPlates")]
-	public IActionResult GetPlates(int page = 0, string orderBy = "id", bool orderAscending = true)
+	public IActionResult GetPlates(int page = 0, string orderBy = "id", bool orderAscending = true, int age = -1, string initials = "")
 	{
-		var plates = 
+		var plates =
 			(orderAscending ? dbContext.Plates.OrderBy(SortBy(orderBy)) : dbContext.Plates.OrderByDescending(SortBy(orderBy)))
 			.ThenBy(x => x.Id)
+			.Where(FilterBy(age, initials))
 			.Skip(page * NUMBER_OF_PLATES)
 			.Take(NUMBER_OF_PLATES)
 			.Select(x => new
@@ -43,5 +44,19 @@ public class PlatesController : Controller
 			"saleprice" => (x => x.SalePrice),
 			_ => (x => x.Id)
 		};
+	}
+
+	private Expression<Func<Plate, bool>> FilterBy(int age, string initials)
+	{
+		if (age > -1 && !string.IsNullOrEmpty(initials.Trim()))
+			return (x => x.Numbers == age && x.Letters.ToLower() == initials.Trim().ToLower());
+
+		if (age > -1 )
+			return (x => x.Numbers == age);
+
+		if (!string.IsNullOrEmpty(initials.Trim()))
+			return (x => x.Letters.ToLower() == initials.Trim().ToLower());
+
+		return (x => true);
 	}
 }
